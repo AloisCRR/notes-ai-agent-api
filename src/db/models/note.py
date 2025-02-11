dbd697a3a056943a11547334b6cbd02ebe2119e4
Alois Carrera <aloiscarrera@gmail.com>
2025-02-11 07:45:53 -0500

    refactor: :recycle: fix wrong date creation

--- src/db/models/note.py
+++ src/db/models/note.py
@@ -1,9 +1,9 @@
-from datetime import datetime, timezone
+from datetime import datetime
 from typing import Optional
 from uuid import UUID
 
 from pgvector.sqlalchemy import Vector
-from sqlalchemy import DateTime
+from sqlalchemy import DateTime, text
 from sqlalchemy.orm import Mapped, mapped_column
 
 from src.db.base import Base
@@ -13,15 +13,17 @@ class Note(Base):
     __tablename__ = "notes"
     __table_args__ = {"schema": "public"}
 
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    user_id: Mapped[Optional[UUID]]
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[UUID]
     content: Mapped[str]
     embedding: Mapped[Optional[Vector]] = mapped_column(Vector)
     created_at: Mapped[datetime] = mapped_column(
-        DateTime(timezone=True), default=datetime.now(timezone.utc)
+        DateTime(timezone=True), server_default=text("(now() AT TIME ZONE 'utc'::text)")
     )
     updated_at: Mapped[datetime] = mapped_column(
-        DateTime(timezone=True), default=datetime.now(timezone.utc)
+        DateTime(timezone=True),
+        server_default=text("(now() AT TIME ZONE 'utc'::text)"),
+        onupdate=text("(now() AT TIME ZONE 'utc'::text)"),
     )
 
     def __repr__(self) -> str:
